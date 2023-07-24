c54911c498606f07a270c07477bdfefe07cd6d47
kasun0403 <85491888+kasun0403@users.noreply.github.com>
2023-07-24 22:05:47 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * header sticky
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 10
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
